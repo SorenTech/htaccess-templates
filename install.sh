53e23c61b0171159efbbc200e04a9421dae9b157
apmarshall <apmarshall@soren.tech>
2015-11-18 20:00:29 -0500

    Updated paths and added new links.

--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@
 # Define our paths
 $ROOT=../../
 $PUBLIC=${ROOT}/public/
-$WPCONTENTS=${ROOT}/vendor
-$WPUPLOADS=${ROOT}/storage/uploads
+$WPCONTENTS=${ROOT}/vendor/
+$WPUPLOADS=${ROOT}/storage/uploads/
+$WPINCLUDES=${ROOT}/app/wp-includes/
+$WPCONFIG=${ROOT}/config/
+$BOOTSTRAP=${ROOT}/bootstrap
 
 # SymLink our files
+ln -s root ${ROOT}/.htaccess
 ln -s wordpress-public ${PUBLIC}/.htaccess
 ln -s wordpress-contents ${WPCONTENTS}/.htaccess
 ln -s wordpress-uploads ${WPUPLOADS}/.htaccess
+ln -s wordpress-includes ${WPINCLUDES}/.htaccess
