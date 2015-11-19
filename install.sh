e37ecc2cf094f3e9a53c09aef470869980358f25
apmarshall <apmarshall@soren.tech>
2015-11-18 20:03:04 -0500

    Updated paths and added a few new files

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ $BOOTSTRAP=${ROOT}/bootstrap
 
 # SymLink our files
 ln -s root ${ROOT}/.htaccess
+ln -s bootstrap ${BOOTSTRAP}/.htaccess
+ln -s wordpress-config ${WPCONFIG}/.htaccess
 ln -s wordpress-public ${PUBLIC}/.htaccess
 ln -s wordpress-contents ${WPCONTENTS}/.htaccess
 ln -s wordpress-uploads ${WPUPLOADS}/.htaccess
